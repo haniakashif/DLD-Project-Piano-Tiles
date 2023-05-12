bc5b48cd50d9f68022a2caf51c76c5af09e5cc65
Hania Kashif <116333070+haniakashif@users.noreply.github.com>
2023-05-12 09:11:01 +0500

    Update ScreenRom.v

--- ScreenRom.v
+++ ScreenRom.v
@@ -1,11 +1,6 @@
-//---------------------------------------------------
-// BeeRom Module - Single Port ROM : Digilent Basys 3
-// BeeInvaders Tutorial 2 : Onboard clock 100MHz
-// VGA Resolution 640x480 @ 60Hz : Pixel Clock 25MHz
-//---------------------------------------------------
 `timescale 1ns / 1ps
-// Setup BeeRom Module
-module BeeRom(
+
+module ScreenRom(
 input wire [16:0] i_addr, // (16:0) or 2^17 or 131072, need 471 x 250 = 117750
 input wire i_clk2,
 output reg [7:0] o_data // (7:0) 8 bit pixel value from Bee.mem
